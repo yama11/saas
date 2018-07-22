430f12107883af387cf86c6c6d4b32dcf8ab36e4
huojinzhao <352782712@qq.com>
2018-07-22 11:55:25 +0800

    feat: init element-ui

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import Vue from 'vue';
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+Vue.use(ElementUI);
 
 /* eslint-disable no-new */
 new Vue({
