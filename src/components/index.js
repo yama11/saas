d425e40a1de3bca3e2f4d1a16f4ea3320315b469
yehaifeng <dreams_ye@163.com>
2018-07-24 14:08:18 +0800

    optimize:组件引用

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,19 @@
 import AppInputPassword from './AppInputPassword';
 import AppFormAlert from './AppFormAlert';
 import AppSearch from './AppSearch';
+import AppList from './AppList';
+import AppForm from './AppForm/Main';
+import AppFormPage from './AppForm/AppFormPage';
+import AppFormDialog from './AppForm/AppFormDialog';
 
 const components = {
   AppInputPassword,
   AppFormAlert,
   AppSearch,
+  AppList,
+  AppForm,
+  AppFormPage,
+  AppFormDialog,
 };
 
 export default (Vue) => {
