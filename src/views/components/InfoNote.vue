380608dee095773bf889a2f57c78dbb4e235a7c0
suyanping <344057827@qq.com>
2018-07-25 14:24:14 +0800

    feat: 便签

---
+++ src/views/components/InfoNote.vue
@@ -0,0 +1,37 @@
+<script>
+/**
+ * @overview  便签
+ *
+ * @author suyanping
+ */
+
+export default {
+  name: 'OrderInfoNote',
+
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+
+    content: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="order-info-note">
+    <span>{{ label }}</span>
+    :
+    <span>{{ content }}</span>
+  </div>
+</template>
+
+<style lang="postcss">
+.order-info-note {
+
+}
+</style>
