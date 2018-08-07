f568fbe1f0bfe9eb5a1608b14bd66647dee14165
huojinzhao <352782712@qq.com>
2018-08-07 19:10:23 +0800

    feat(Subject/Edition)

--- src/views/Subject/Edition.vue
+++ src/views/Subject/Edition.vue
@@ -7,17 +7,224 @@
 
 export default {
   name: 'SubjectEdition',
+
+  data: () => ({
+    subject: {
+      name: '',
+      id: 0,
+    },
+
+    data: [],
+
+    dialogVisible: false,
+
+    formData: {
+      name: '',
+      subject_id: 0,
+      parent_id: 0,
+      describe: '',
+    },
+
+    rules: {
+      name: [
+        { required: true, message: '请填写学科名称' },
+      ],
+    },
+
+    treeProps: {
+      label: 'name',
+      children: 'leaf',
+    },
+
+    editTargetID: null,
+  }),
+
+  computed: {
+    subjectID() {
+      return Number(this.$route.params.id);
+    },
+  },
+
+  created() {
+    this.getBefore();
+
+    this.getData();
+  },
+
+  methods: {
+    getBefore() {
+      this.$http.get('/structure/index_before')
+        .then(({ subject: subjects }) => {
+          this.subject = subjects
+            .find(item => item.id === this.subjectID);
+        });
+    },
+
+    getData() {
+      const url = `/structure?equal[subject_id]=${this.subjectID}`;
+
+      this.$http.get(url)
+        .then(({ structure }) => {
+          this.data = structure;
+        });
+    },
+
+    preLeafCreate(parentID = 0) {
+      this.editTargetID = null;
+
+      this.formData = {
+        name: '',
+        describe: '',
+        subject_id: this.subjectID,
+        parent_id: parentID,
+      };
+
+      this.dialogVisible = true;
+    },
+
+    structureCreate(submit) {
+      submit()
+        .then(this.getData);
+    },
+
+    deleteNode(node, data) {
+      const url = `/structure/${data.id}`;
+
+      this.$confirm(
+        '确定删除该菜单么？',
+        '删除确认',
+        {
+          showCancelButton: true,
+          type: 'warning',
+          confirmButtonText: '删除',
+          confirmButtonClass: 'el-button--danger',
+        })
+        .then(() => this.$http.delete(url))
+        .then(this.getData)
+        .catch(() => {
+          this.$message.error('删除菜单失败!');
+        });
+    },
+
+    appendNode(node, data) {
+      this.preLeafCreate(data.id);
+    },
+
+    editNode({
+      id,
+      name,
+      subject_id,
+      parent_id,
+      describe = '',
+    }) {
+      this.editTargetID = id;
+
+      this.formData = {
+        id,
+        name,
+        subject_id,
+        parent_id,
+        describe,
+      };
+
+      this.dialogVisible = true;
+    },
+  },
 };
 </script>
 
 <template>
-  <div class="subject-edition">
-    学科管理 - 学科结构编辑
+  <div class="global-main subject-edition">
+    <h2>
+      <span>{{ subject.name }}</span>
+      <el-button
+        type="primary"
+        @click="() => preLeafCreate()"
+      >
+        添加菜单
+      </el-button>
+    </h2>
+
+    <el-tree
+      :props="treeProps"
+      :data="data"
+      node-key="id"
+      children="leaf"
+    >
+      <div
+        slot-scope="{ node, data }"
+        class="subject-edition__node"
+      >
+        <span>{{ node.label }}</span>
+        <span class="subject-edition__node-control">
+          <el-button
+            type="text"
+            size="mini"
+            @click="() => editNode(data)"
+          >
+            编辑
+          </el-button>
+          <el-button
+            :style="{
+              visibility: node.isLeaf ? 'visible' : 'hidden'
+            }"
+            type="text"
+            size="mini"
+            @click="() => deleteNode(node, data)"
+          >
+            删除
+          </el-button>
+          <el-button
+            type="text"
+            size="mini"
+            @click="() => appendNode(node, data)"
+          >
+            +添加子菜单
+          </el-button>
+        </span>
+      </div>
+    </el-tree>
+
+    <AppFormDialog
+      :model="formData"
+      :rules="rules"
+      :visible.sync="dialogVisible"
+      :id="editTargetID"
+      label-width="6em"
+      width="500px"
+      url="/structure"
+      object="添加学科"
+      @on-submit="structureCreate"
+    >
+      <el-form-item
+        label="菜单名称"
+        prop="name"
+      >
+        <el-input v-model="formData.name" />
+      </el-form-item>
+    </AppFormDialog>
   </div>
 </template>
 
 <style lang="postcss">
 .subject-edition {
+  padding-top: 0;
+}
+
+.subject-edition > h2 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.subject-edition__node {
+  display: inline-flex;
+  width: 700px;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.subject-edition__node-control {
 
 }
 </style>
---
+++ src/views/Subject/index.vue
@@ -0,0 +1,141 @@
+<script>
+/**
+ * @overview 学科管理 - 学科列表
+ *
+ * @author huojinzhao
+ */
+
+import { list } from '@/mixins';
+import ListItem from './components/ListItem';
+
+export default {
+  name: 'SubjectList',
+
+  components: {
+    ListItem,
+  },
+
+  mixins: [list],
+
+  data: () => ({
+    list: [],
+
+    dialogVisible: false,
+
+    formData: {
+      name: '',
+    },
+
+    rules: {
+      name: [
+        { required: true, message: '请填写学科名称' },
+      ],
+    },
+  }),
+
+  created() {
+    this.getList();
+  },
+
+  methods: {
+    getList() {
+      this.$_listMixin_getList('/subject')
+        .then(({ data }) => {
+          this.list = data;
+        });
+    },
+
+    preCreateSubject() {
+      this.dialogVisible = true;
+    },
+
+    subjectCreate(submit) {
+      submit()
+        .then(this.getList);
+    },
+
+    subjectDelete(id) {
+      this.$_listMixin_alertDeleteItem(
+        id,
+        this.list,
+        '学科',
+        '/subject',
+      );
+    },
+
+    subjectEdit(id) {
+      this.$router.push(`/subject-edition/${id}`);
+    },
+  },
+};
+</script>
+
+<template>
+  <div
+    v-loading="loading"
+    class="global-main subject-list"
+  >
+    <h2 class="subject-list__title">学科管理</h2>
+    <div class="subject-list__body">
+      <ListItem
+        v-for="item in list"
+        :key="item.id"
+        :data="item"
+        @delete="subjectDelete"
+        @edit="subjectEdit"
+      />
+      <div
+        class="module-subject__list-item subject-list__create"
+        @click="preCreateSubject"
+      >
+        <i class="el-icon-plus" />
+      </div>
+    </div>
+
+    <AppFormDialog
+      :model="formData"
+      :rules="rules"
+      :visible.sync="dialogVisible"
+      label-width="6em"
+      width="500px"
+      url="/subject"
+      object="添加学科"
+      @on-submit="subjectCreate"
+    >
+      <el-form-item
+        label="学科名称"
+        prop="name"
+      >
+        <el-input v-model="formData.name" />
+      </el-form-item>
+    </AppFormDialog>
+  </div>
+</template>
+
+<style lang="postcss">
+.subject-list {
+
+}
+
+.subject-list__title {
+
+}
+
+.subject-list__body {
+  flex-grow: 1;
+  display: flex;
+  align-content: flex-start;
+  flex-wrap: wrap;
+}
+
+div.subject-list__create {
+  font-size: 48px;
+}
+
+.subject-list__create:hover {
+  cursor: pointer;
+  color: var(--color-primary);
+}
+</style>
+
+<style src="./index.css" lang="postcss"></style>
