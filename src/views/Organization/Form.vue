1f29f4ace1ff22b90e87445f8a61ab668e86c6b1
yehaifeng <dreams_ye@163.com>
2018-08-20 10:10:49 +0800

    optimizr :机构管理样式修改

--- src/views/Organization/Form.vue
+++ src/views/Organization/Form.vue
@@ -103,153 +103,153 @@ export default {
     :object="id ?'编辑机构':'添加机构'"
     label-width="6em"
     url="/department"
-    class="organization-form"
     @on-submit="submitEdition"
     @on-cancel="cancelForm"
-
   >
-    <el-form-item
-      label="机构名称"
-      prop="name"
-    >
-      <el-input
-        v-model="formDate.name"
-        :maxlength="20"
-        placeholder="请输入机构名称"
-      />
-    </el-form-item>
-    <el-form-item
-      label="机构编码"
-      prop="number"
-    >
-      <el-input
-        v-model="formDate.number"
-        :maxlength="10"
-        placeholder="请输入机构编码"
-      />
-    </el-form-item>
-    <el-form-item
-      label="所属经销商"
-      prop="dealer_id"
-      class="organization-form__dealer"
-    >
-      <el-select
-        v-model="formDate.dealer_id"
-        placeholder="请输入所属经销商"
-      >
-        <el-option
-          v-for="role in deBefore"
-          :key="role.id"
-          :label="role.name"
-          :value="role.id"
-        />
-      </el-select>
-    </el-form-item>
-    <el-form-item
-      label="联系电话"
-      prop="phone"
-    >
-      <el-input
-        v-model="formDate.phone"
-        placeholder="请输入联系电话"
-      />
-    </el-form-item>
-    <el-form-item
-      label="管理员账号"
-      prop="account"
-    >
-      <el-input
-        v-model="formDate.account"
-        placeholder="请输入管理员账号"
-      />
-    </el-form-item>
-    <el-form-item
-      label="密码"
-      prop="password"
-    >
-      <el-input
-        v-model="formDate.password"
-        placeholder="请输入密码"
-      />
-    </el-form-item>
-    <el-form-item
-      prop="places"
-      label="所在区域"
-    >
-      <el-cascader
-        :options="chinaJSON"
-        v-model="formDate.places"
-        placeholder="请选择所在地区"
-      />
-    </el-form-item>
-    <el-form-item
-      label="详细地址"
-      prop="address"
-    >
-      <el-input
-        v-if="address"
-        v-model="address"
-        placeholder="请输入详细地址"
-      />
-      <el-input
-        v-else
-        v-model="formDate.address"
-        placeholder="请输入详细地址"
-      />
-    </el-form-item>
     <div
-      class="organization-form__map"
+      class="organization-form"
     >
-      <baidu-map
-        id="alimap"
-        :center="center"
-        class="map"
-        ak="T9kb9PgCGdvSBOiHW7vMbEmU0S8gyTjK"
-        @ready="handler"
-      />
-      <el-form-item
-        label="经纬度"
-        prop="longitude"
+      <div
+        class="organization-form__data"
       >
-        <div
-          v-if="id"
+        <el-form-item
+          label="机构名称"
+          prop="name"
         >
-          {{ formDate.longitude }},{{ formDate.latitude }}
-        </div>
-        <div
-          v-else
+          <el-input
+            v-model="formDate.name"
+            :maxlength="20"
+            placeholder="请输入机构名称"
+          />
+        </el-form-item>
+        <el-form-item
+          label="机构编码"
+          prop="number"
         >
-          {{ mapLngLat }}
-        </div>
-      </el-form-item>
+          <el-input
+            v-model="formDate.number"
+            :maxlength="10"
+            placeholder="请输入机构编码"
+          />
+        </el-form-item>
+        <el-form-item
+          label="所属经销商"
+          prop="dealer_id"
+          class="organization-form__dealer"
+        >
+          <el-select
+            v-model="formDate.dealer_id"
+            placeholder="请输入所属经销商"
+          >
+            <el-option
+              v-for="role in deBefore"
+              :key="role.id"
+              :label="role.name"
+              :value="role.id"
+            />
+          </el-select>
+        </el-form-item>
+        <el-form-item
+          label="联系电话"
+          prop="phone"
+        >
+          <el-input
+            v-model="formDate.phone"
+            placeholder="请输入联系电话"
+          />
+        </el-form-item>
+        <el-form-item
+          label="管理员账号"
+          prop="account"
+        >
+          <el-input
+            v-model="formDate.account"
+            placeholder="请输入管理员账号"
+          />
+        </el-form-item>
+        <el-form-item
+          label="密码"
+          prop="password"
+        >
+          <el-input
+            v-model="formDate.password"
+            placeholder="请输入密码"
+          />
+        </el-form-item>
+        <el-form-item
+          prop="places"
+          label="所在区域"
+        >
+          <el-cascader
+            :options="chinaJSON"
+            v-model="formDate.places"
+            placeholder="请选择所在地区"
+          />
+        </el-form-item>
+        <el-form-item
+          label="详细地址"
+          prop="address"
+        >
+          <el-input
+            v-if="address"
+            v-model="address"
+            placeholder="请输入详细地址"
+          />
+          <el-input
+            v-else
+            v-model="formDate.address"
+            placeholder="请输入详细地址"
+          />
+        </el-form-item>
+      </div>
+      <div
+        class="organization-form__map"
+      >
+        <baidu-map
+          id="alimap"
+          :center="center"
+          class="map"
+          ak="T9kb9PgCGdvSBOiHW7vMbEmU0S8gyTjK"
+          @ready="handler"
+        />
+        <el-form-item
+          label="经纬度"
+          prop="longitude"
+        >
+          <div
+            v-if="id"
+          >
+            {{ formDate.longitude }},{{ formDate.latitude }}
+          </div>
+          <div
+            v-else
+          >
+            {{ mapLngLat }}
+          </div>
+        </el-form-item>
+      </div>
     </div>
   </AppFormPage>
 </template>
 <style lang="postcss">
-.organization-form .el-form-item{
-
-  width: 34%
-}
 .organization-form .el-cascader,
 .organization-form__dealer .el-select{
   width: 100%;
 }
-.organization-form{
-  position: relative;
-}
-.organization-form .organization-form__map{
-    position: absolute;
-    width: 650px;
-    height: 350px;
-    right: 250px;
-    top: 62px;
-}
 .organization-form .organization-form__map .el-form-item{
   font-size: 16px;
 }
+.organization-form{
+  display: flex;
+  justify-content: space-between;
+}
+.organization-form__data{
+  width:400px;
+}
 .map{
   width: 650px;
-  height: 300px;
+  height: 350px;
+  margin-right: 200px;
 }
 .BMap_cpyCtrl {
   display: none;
