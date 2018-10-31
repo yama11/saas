43e398aaa66a44a0557e0ba7d06afbba5fa0b3d4
suyanping <344057827@qq.com>
2018-10-31 15:04:48 +0800

    feat(Video): 视频配置

---
+++ src/views/Video/VideoItem.vue
@@ -0,0 +1,146 @@
+<script>
+/**
+ * @overview 家长端管理 - 视频配置 - 视频item
+ *
+ * @author suyanping
+ */
+
+export default {
+  name: 'VideoItem',
+
+  props: {
+    data: {
+      type: Object,
+      default: () => {},
+    },
+  },
+
+  data() {
+    return {
+
+    };
+  },
+
+  methods: {
+    getLinkVideo(link) {
+      if (link) {
+        window.open(link);
+      }
+    },
+
+    updateEvent(id) {
+      this.$emit('getUpdate', id);
+    },
+
+    deleteEvent(id) {
+      this.$emit('getDelete', id);
+    },
+
+    shelveEvent(id) {
+      this.$emit('getShelve', id);
+    },
+
+    offShelveEvent(id) {
+      this.$emit('getOffShelve', id);
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="video-item">
+    <div
+      class="video-item__image"
+      @click="getLinkVideo(data.link)">
+      <img :src="data.cover_image">
+    </div>
+
+    <div class="video-item__content">
+      <div class="video-item__title">
+        {{ data.title }}
+      </div>
+
+      <div class="video-item__desc">
+        {{ data.description }}
+      </div>
+
+      <div class="video-item__action">
+
+        <i
+          v-if="data.video_status === 3"
+          class="el-icon-upload2"
+          @click="shelveEvent(data.id)">
+          上架
+        </i>
+
+        <i
+          v-if="data.video_status === 2"
+          class="el-icon-download"
+          @click="offShelveEvent(data.id)">
+          下架
+        </i>
+
+        <i
+          class="el-icon-edit"
+          @click="updateEvent(data.id)">
+          编辑
+        </i>
+
+        <i
+          class="el-icon-delete"
+          @click="deleteEvent(data.id)">
+          删除
+        </i>
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<style lang="postcss">
+.video-item{
+  display: inline-block;
+  width: 250px;
+  height: 270px;
+  border: 1px solid gainsboro;
+  margin: 20px 20px 0 0;
+  position: relative;
+}
+
+.video-item__image{
+  cursor: pointer;
+}
+
+.video-item__image img{
+  width: 100%;
+  height: 150px;
+  border-bottom: 1px solid gainsboro;
+}
+
+.video-item__content{
+  height: 120px;
+  padding: 15px 0px 5px 10px;
+}
+
+.video-item__title{
+  font-size: 15px;
+}
+
+.video-item__desc{
+  font-size: 13px;
+  color: darkgray;
+}
+
+.video-item__action{
+  width: 90%;
+  display: flex;
+  justify-content: space-around;
+  position: absolute;
+  bottom: 5px;
+}
+
+.video-item__action i{
+  cursor: pointer;
+}
+</style>
--- src/views/Video/index.vue
+++ src/views/Video/index.vue
@@ -4,30 +4,227 @@
  *
  * @author suyanping
  */
+import list from '@/mixins/list';
+import errorHandler from '@/components/AppFormAlert/errorHandler';
+import VideoItem from './VideoItem';
 
 export default {
   name: 'VideoList',
 
+  components: {
+    VideoItem,
+  },
+
+  mixins: [list],
+
   data() {
     return {
+      list: {},
 
+      formData: {
+        title: '',
+        description: '',
+        cover_image: '',
+        link: '',
+      },
+
+      formRules: {
+        title: [
+          this.$rules.required('标题'),
+        ],
+
+        description: [
+          this.$rules.required('描述'),
+        ],
+
+        cover_image: [
+          this.$rules.required('封面图'),
+        ],
+      },
+
+      videoId: null,
+
+      visible: false,
     };
   },
 
-  created() {
-
-  },
-
   methods: {
+    checkPermission(key, text) {
+      return this.$permissions(`client.video.${key}`, text);
+    },
 
+    createVideo() {
+      this.visible = true;
+
+      this.formData = {
+        title: '',
+        description: '',
+        cover_image: '',
+        link: '',
+      };
+    },
+
+    updateVideo(id) {
+      this.videoId = id;
+
+      this.visible = true;
+
+      this.$http.get(`/video/${id}`)
+        .then((res) => {
+          this.formData = {
+            title: res.title,
+            description: res.description,
+            cover_image: res.cover_image,
+            link: res.link,
+          };
+        })
+        .catch((error) => {
+          const errorMessage = errorHandler(error);
+
+          this.$message.error(errorMessage[0]);
+        });
+    },
+
+    deleteVideo(id) {
+      this.$_listMixin_alertDeleteItem(
+        id,
+        this.list.data,
+        '视频',
+        '/video',
+        this.$refs.list,
+      );
+    },
+
+    shelveVideo(id) {
+      this.$confirm(
+        '确定上架该视频？',
+        '确认上架',
+        {
+          showCancelButton: true,
+          type: 'primary',
+          confirmButtonText: '上架',
+          confirmButtonClass: 'el-button--primary',
+        })
+        .then(() => {
+          this.sureShelve(id);
+        })
+        .catch(() => {});
+    },
+
+    sureShelve(id) {
+      this.$http.post(`/video/${id}/shelve`)
+        .then(() => this.$refs.list.getList())
+        .catch((error) => {
+          const errorMessage = errorHandler(error);
+
+          this.$message.error(errorMessage[0]);
+        });
+    },
+
+    offShelveVideo(id) {
+      this.$http.post(`/video/${id}/off_shelve`)
+        .then(() => this.$refs.list.getList())
+        .catch((error) => {
+          const errorMessage = errorHandler(error);
+
+          this.$message.error(errorMessage[0]);
+        });
+    },
+
+    getCoverUrl(url) {
+      this.formData.cover_image = url;
+    },
+
+    submitEdition(submit) {
+      submit()
+        .then(() => this.$refs.list.getList())
+        .catch((error) => {
+          const errorMessage = errorHandler(error);
+
+          this.$message.error(errorMessage[0]);
+        });
+    },
   },
 };
 </script>
 
 <template>
-  <div class="video-list">
-    视频配置
-  </div>
+  <AppList
+    ref="list"
+    :list.sync="list"
+    :create-label="checkPermission('store') ? '添加视频' : null"
+    api="/video"
+    title="视频配置"
+    @create="createVideo"
+  >
+
+    <template
+      v-if="checkPermission('index')"
+      slot-scope="props"
+    >
+      <VideoItem
+        v-for="item in props.listData"
+        :key="item.id"
+        :data="item"
+        @getUpdate="updateVideo"
+        @getDelete="deleteVideo"
+        @getShelve="shelveVideo"
+        @getOffShelve="offShelveVideo"/>
+
+      <AppFormDialog
+        :visible.sync="visible"
+        :rules="formRules"
+        :model="formData"
+        :id="videoId"
+        :object="videoId ? '编辑视频' : '添加视频'"
+        url="/video"
+        label-width="6em"
+        width="800px"
+        @on-submit="submitEdition"
+      >
+        <el-form-item
+          prop="title"
+          label="标题"
+        >
+          <el-input
+            v-model="formData.title"
+            :maxlength="15"
+            placeholder="请输入标题"
+          />
+        </el-form-item>
+
+        <el-form-item
+          prop="description"
+          label="描述"
+        >
+          <el-input
+            v-model="formData.description"
+            :maxlength="50"
+            placeholder="请输入描述"
+          />
+        </el-form-item>
+
+        <el-form-item
+          prop="cover_image"
+          label="封面图">
+          <AppUploadScale
+            v-model="formData.cover_image"
+            @on-success="getCoverUrl"/>
+        </el-form-item>
+
+        <el-form-item
+          prop="link"
+          label="视频链接"
+        >
+          <el-input
+            v-model="formData.link"
+            placeholder="请输入视频链接"
+          />
+        </el-form-item>
+      </AppFormDialog>
+    </template>
+
+  </AppList>
 </template>
 
 <style lang="postcss">
